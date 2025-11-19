ff308e2b354423ca8da918efa9087b8c4bcfd5f7
scottchayaa <mmx112945@gmail.com>
2025-11-19 11:46:01 +0800

    refactor: 簡化 auth.js validator 寫法

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,21 +4,26 @@ const { body } = require('express-validator');
 const { register, login } = require('../controllers/authController');
 const { validate } = require('../middleware/validator');
 
-/**
- * 公開 API 路由 - 認證
- */
+const bodyEmailValidator = () => body('email')
+      .notEmpty().withMessage('Email 為必填欄位')
+      .isEmail().withMessage('Email 格式不正確');
+
+const bodyPasswordValidator = () => body('password')
+      .notEmpty().withMessage('密碼為必填欄位')
+      .isLength({ min: 6 }).withMessage('密碼至少需要 6 個字元');
 
 // 註冊
 // POST /api/auth/register
 router.post(
   '/register',
   [
-    body('email').isEmail().withMessage('Email 格式不正確'),
-    body('password')
-      .isLength({ min: 6 })
-      .withMessage('密碼至少需要 6 個字元'),
-    body('name').notEmpty().withMessage('姓名為必填欄位'),
-    body('phone').notEmpty().isMobilePhone('zh-TW').withMessage('請輸入正確的台灣手機號碼'),
+    bodyEmailValidator(),
+    bodyPasswordValidator(),
+    body('name')
+      .notEmpty().withMessage('姓名為必填欄位'),
+    body('phone')
+      .notEmpty().withMessage('電話為必填欄位')
+      .isMobilePhone('zh-TW').withMessage('請輸入正確的台灣手機號碼'),
     validate,
   ],
   register
@@ -29,8 +34,8 @@ router.post(
 router.post(
   '/login',
   [
-    body('email').isEmail().withMessage('Email 格式不正確'),
-    body('password').notEmpty().withMessage('密碼為必填欄位'),
+    bodyEmailValidator(),
+    bodyPasswordValidator(),
     validate,
   ],
   login
